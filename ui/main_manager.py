fc8af5e23de10c5f539d9afb40e6deda54c90376
jihyoo0205 <jihyoo0205@gmail.com>
2023-07-09 19:54:06 +0900

    +, - 버튼 구현

--- ui/main_manager.py
+++ ui/main_manager.py
@@ -1,4 +1,6 @@
 # https://doc.qt.io/qtforpython-5/tutorials/basictutorial/uifiles.html
+# https://hello-bryan.tistory.com/407
+# https://onlytojay.medium.com/pyside2로-간단한-calcultor-exe-만들기-3cf247b21f6e
 
 from PySide2.QtCore import *
 from PySide2.QtGui import *
@@ -18,7 +20,16 @@ class MainWindow(QObject):
         loader = QUiLoader()
         self.window = loader.load(uiFile)
         uiFile.close()
+        self.setup_ui()
         self.window.show()
+    
+    def setup_ui(self):
+        self.treeSrcTab = self.__bindQTreeWidget('treeViewSrcTables')
+        self.treeMigTab = self.__bindQTreeWidget('treeViewMigTables')
+        self.pbPlus = self.__bindQPushButton('pushButtonPlus')
+        self.pbMinus = self.__bindQPushButton('pushButtonMinus')
+        self.pbPlus.clicked.connect(self.copy_item)
+        self.pbMinus.clicked.connect(self.remove_item)
 
     def __bindQLineEdit(self, objectName):
         return self.window.findChild(QLineEdit, objectName)
@@ -43,6 +54,103 @@ class MainWindow(QObject):
         # 버튼 클릭했을 때 click_objectName 함수 호출
         #eval(f'btn.clicked.connect(self.click_{objectName})')
         return btn
+    
+    def find_item(self, treeList, item):
+        count = treeList.topLevelItemCount()
+        for i in range(count):
+            if item.text(0) == treeList.topLevelItem(i).text(0):
+                return i
+            
+        return -1
+    
+    def find_child_items(self, parent_item, selected_item):
+        count = parent_item.childCount()
+        for i in range(count):
+            child_item = parent_item.child(i)
+            if selected_item.text(0) == child_item.text(0):
+                return i
+        return -1
+
+    def copy_item(self):
+        sender = self.sender()
+        if self.pbPlus == sender:
+            srcTreeList = self.treeSrcTab
+            tgtTreeList = self.treeMigTab
+        else:
+            srcTreeList = self.treeMigTab
+            tgtTreeList = self.treeSrcTab
+
+        selected_items = srcTreeList.selectedItems()
+
+        # 선택했음
+        for item in selected_items:
+            parent_item = item.parent()
+            child_count = item.childCount()
+            copied_item = item.clone()
+
+            # 1) 최상단 항목일 때
+            if parent_item is None:
+                if self.find_item(tgtTreeList, copied_item) < 0:
+                    tgtTreeList.addTopLevelItem(copied_item)
+
+            # 2) 하위 항목일 때
+            else:
+                # 선택한 항목이 최하위 항목일 때
+                if child_count == 0:
+                    copied_parent_item = parent_item.clone()
+                    copied_parent_item.takeChildren()
+                    retrunIdx = self.find_item(tgtTreeList, copied_parent_item)
+
+                    # 같은 스키마가 없으면 추가
+                    if retrunIdx < 0:
+                        copied_parent_item.addChild(copied_item)
+                        tgtTreeList.addTopLevelItem(copied_parent_item)
+
+                    # 있으면 그 아래에 하위 항목만추가
+                    else:
+                        target_parent_item = tgtTreeList.topLevelItem(retrunIdx)
+                        if self.find_child_items(target_parent_item, copied_item) < 0:
+                            target_parent_item.addChild(copied_item)
+
+
+    def remove_item(self):
+        sender = self.sender()
+        if self.pbPlus == sender:
+            srcTreeList = self.treeSrcTab
+            tgtTreeList = self.treeMigTab
+        else:
+            srcTreeList = self.treeMigTab
+            tgtTreeList = self.treeSrcTab
+
+        selected_items = srcTreeList.selectedItems()
+
+        for item in selected_items:
+            parent_item = item.parent()
+            if parent_item is not None:
+                if parent_item.childCount() == 1:
+                    index = srcTreeList.indexOfTopLevelItem(parent_item)
+                    srcTreeList.takeTopLevelItem(index)
+                parent_item.removeChild(item)
+            else:
+                index = srcTreeList.indexOfTopLevelItem(item)
+                srcTreeList.takeTopLevelItem(index)
+
+
+
+    def clickMoveItem(self):
+        sender = self.sender()
+        if self.pbPlus == sender:
+            srcTreeList = self.treeSrcTab
+            tgtTreeList = self.treeMigTab
+        else:
+            srcTreeList = self.treeMigTab
+            tgtTreeList = self.treeSrcTab
+
+
+        item = srcTreeList.takeTopLevelItem(srcTreeList.currentColumn())
+        root = QTreeWidget.invisibleRootItem(tgtTreeList)
+        root.addChild(item)
+
 
 def exec():
     app = QApplication(sys.argv)
